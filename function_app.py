47ce2b62bc4650cb7ba73b8e3db4e3059d676e61
Tomas Staškevičius <akurat2012@gmail.com>
2024-02-22 22:17:22 +0200

    Add speech-to-text functionality using OpenAI Whisper model

--- function_app.py
+++ function_app.py
@@ -3,14 +3,37 @@ import logging
 # import os
 # import tempfile
 # import requests
-import whisper
-# from urllib.parse import quote
+#import whisper
+# from urllib.parse import 
+import torch
+from transformers import AutoModelForSpeechSeq2Seq, AutoProcessor, pipeline
 
 app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 
 @app.route(route="whisperai")
 def whisperai(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
+
+    # Initialize the model and processor
+    device = "cuda:0" if torch.cuda.is_available() else "cpu"
+    torch_dtype = torch.float16 if torch.cuda.is_available() else torch.float32
+    model_id = "openai/whisper-large-v3"
+    model = AutoModelForSpeechSeq2Seq.from_pretrained(
+        model_id, torch_dtype=torch_dtype, low_cpu_mem_usage=True, use_safetensors=True
+    ).to(device)
+    processor = AutoProcessor.from_pretrained(model_id)
+    pipe = pipeline(
+        "automatic-speech-recognition",
+        model=model,
+        tokenizer=processor.tokenizer,
+        feature_extractor=processor.feature_extractor,
+        max_new_tokens=128,
+        chunk_length_s=30,
+        batch_size=16,
+        return_timestamps=True,
+        torch_dtype=torch_dtype,
+        device=device,
+    )
 
     name = req.params.get('name')
     # file_url = req.params.get('file')
