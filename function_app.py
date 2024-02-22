a546168b67cf6bd368a2f69dafe5ecd17f959a70
Tomas Staškevičius <akurat2012@gmail.com>
2024-02-22 21:17:20 +0200

    Add file transcription functionality

--- function_app.py
+++ function_app.py
@@ -1,5 +1,10 @@
 import azure.functions as func
 import logging
+import os
+import tempfile
+import requests
+import whisper
+from urllib.parse import quote
 
 app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 
@@ -8,6 +13,7 @@ def whisperai(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
     name = req.params.get('name')
+    file_url = req.params.get('file')
     if not name:
         try:
             req_body = req.get_json()
@@ -15,9 +21,15 @@ def whisperai(req: func.HttpRequest) -> func.HttpResponse:
             pass
         else:
             name = req_body.get('name')
+            file_url = req_body.get('file')
 
     if name:
         return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
+    elif file_url:
+        model = whisper.load_model("base")
+        result = model.transcribe(file_url)
+        my_result = result["text"]
+        return func.HttpResponse(f"{my_result}")
     else:
         return func.HttpResponse(
              "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
