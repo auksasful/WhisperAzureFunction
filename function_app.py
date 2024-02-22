20ae1ce8bb2cd230507a655a9071cc20c56dc7d3
Tomas Staškevičius <akurat2012@gmail.com>
2024-02-22 22:23:10 +0200

    Commented out model initialization and pipeline creation in whisperai function

--- function_app.py
+++ function_app.py
@@ -15,25 +15,25 @@ def whisperai(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
     # Initialize the model and processor
-    device = "cuda:0" if torch.cuda.is_available() else "cpu"
-    torch_dtype = torch.float16 if torch.cuda.is_available() else torch.float32
-    model_id = "openai/whisper-large-v3"
-    model = AutoModelForSpeechSeq2Seq.from_pretrained(
-        model_id, torch_dtype=torch_dtype, low_cpu_mem_usage=True, use_safetensors=True
-    ).to(device)
-    processor = AutoProcessor.from_pretrained(model_id)
-    pipe = pipeline(
-        "automatic-speech-recognition",
-        model=model,
-        tokenizer=processor.tokenizer,
-        feature_extractor=processor.feature_extractor,
-        max_new_tokens=128,
-        chunk_length_s=30,
-        batch_size=16,
-        return_timestamps=True,
-        torch_dtype=torch_dtype,
-        device=device,
-    )
+    # device = "cuda:0" if torch.cuda.is_available() else "cpu"
+    # torch_dtype = torch.float16 if torch.cuda.is_available() else torch.float32
+    # model_id = "openai/whisper-large-v3"
+    # model = AutoModelForSpeechSeq2Seq.from_pretrained(
+    #     model_id, torch_dtype=torch_dtype, low_cpu_mem_usage=True, use_safetensors=True
+    # ).to(device)
+    # processor = AutoProcessor.from_pretrained(model_id)
+    # pipe = pipeline(
+    #     "automatic-speech-recognition",
+    #     model=model,
+    #     tokenizer=processor.tokenizer,
+    #     feature_extractor=processor.feature_extractor,
+    #     max_new_tokens=128,
+    #     chunk_length_s=30,
+    #     batch_size=16,
+    #     return_timestamps=True,
+    #     torch_dtype=torch_dtype,
+    #     device=device,
+    # )
 
     name = req.params.get('name')
     # file_url = req.params.get('file')
